6f34bbb3ce226884199afd427194e7be455baf05
thomas3687 <thomas@tandt-web.de>
2014-02-27 11:47:59 +0100

    Update localconfig.php

--- system/config/localconfig.php
+++ system/config/localconfig.php
@@ -3,18 +3,18 @@
 
 ### INSTALL SCRIPT START ###
 $GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
-$GLOBALS['TL_CONFIG']['installPassword'] = '$6$37b54db496799b73$4jjBWijgV4mVk17GItiuqHdJkPt0acR4eLa/eO2W3Qm8RTOI5lOnbDidpXDnXseeo6RpWnk0slZP9BzKRuzSZ/';
+$GLOBALS['TL_CONFIG']['installPassword'] = '';
 $GLOBALS['TL_CONFIG']['encryptionKey'] = '447f95833aa364b21e4bf377e68c74a5';
-$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQLi';
-$GLOBALS['TL_CONFIG']['dbHost'] = 'localhost';
-$GLOBALS['TL_CONFIG']['dbUser'] = 'root';
-$GLOBALS['TL_CONFIG']['dbPass'] = 'redwood';
-$GLOBALS['TL_CONFIG']['dbDatabase'] = 'astoria_contao';
-$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
-$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
-$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['dbDriver'] = '';
+$GLOBALS['TL_CONFIG']['dbHost'] = '';
+$GLOBALS['TL_CONFIG']['dbUser'] = '';
+$GLOBALS['TL_CONFIG']['dbPass'] = '';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = '';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = ;
+$GLOBALS['TL_CONFIG']['dbCharset'] = '';
+$GLOBALS['TL_CONFIG']['dbPort'] = ;
 $GLOBALS['TL_CONFIG']['dbSocket'] = '';
-$GLOBALS['TL_CONFIG']['adminEmail'] = 'thomas@bilich.net';
+$GLOBALS['TL_CONFIG']['adminEmail'] = '';
 $GLOBALS['TL_CONFIG']['debugMode'] = false;
 $GLOBALS['TL_CONFIG']['defaultUser'] = '';
 $GLOBALS['TL_CONFIG']['defaultGroup'] = '';
